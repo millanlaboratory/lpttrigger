8b04c4514837e240003e49a164ee57b39efdd371
Nicolas Bourdaud <nbourdau@tapadufrix.(none)>
2008-05-16 14:16:59 +0200

    remove windows endline

--- lptsynchro.h
+++ lptsynchro.h
@@ -2,8 +2,10 @@
 #define LPTSYNCHRO_H
 
 struct lptsynchro;
-struct lptsynchro* OpenLPTSynchro(uint32_t base_level, unsigned int duration);
-CloseLPTSynchro(struct lptsynchro* synchro);
-int SetSynchroDuration(struct lptsynchro* synchro, unsigned int duration);
+struct lptsynchro *OpenLPTSynchro(uint32_t base_level,
+				  unsigned int duration);
+CloseLPTSynchro(struct lptsynchro *synchro);
+int SetSynchroDuration(struct lptsynchro *synchro, unsigned int duration);
+unsigned long SignalSynchro(struct lptsynchro *synchro, unsigned int message);
 
-#endif	//LPTSYNCHRO_H
+#endif				//LPTSYNCHRO_H
