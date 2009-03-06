a8960a6b49824ee74b8ac5143a96674b2fab53d7
Nicolas Bourdaud <nicolas.bourdaud@epfl.ch>
2009-03-06 15:47:05 +0100

    Make the lib C compatible if compiled in C++

--- src/lpttrigger.h
+++ src/lpttrigger.h
@@ -11,11 +11,18 @@
 #ifndef _LPTTRIGGER_H_
 #define _LPTTRIGGER_H_
 
+#ifdef __cplusplus
+extern "C" {
+#endif
 
 struct lpttrigger;
 
 struct lpttrigger *OpenLPTTrigger(unsigned char base_level, unsigned int duration, int portnum); //!< Open the LPT port and allocate the ressource
 void CloseLPTTrigger(struct lpttrigger *trigg); //!< Close the connection to the port and free ressources
 void SignalTrigger(struct lpttrigger *trigg, unsigned int message); //!< Send a trigger signal
+
+#ifdef __cpluplus
+}
+#endif
 
 #endif //_LPTTRIGGER_H_
