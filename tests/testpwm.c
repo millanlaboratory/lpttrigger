634e41508771f7cf52ad1fb3438f122fede890a8
bourdaud <bourdaud@3b4714c8-b19d-487d-9711-26c957be2895>
2009-07-17 14:53:57 +0000

    Add missing tests
    git-svn-id: https://svn.epfl.ch/svn/activetwotools/trunk/input-output/lpttrigger@67 3b4714c8-b19d-487d-9711-26c957be2895

---
+++ tests/testpwm.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <lptpwm.h>
+#include <unistd.h>
+
+
+#define NUMCH	4
+#define FREQ	1000
+
+int main(int argc, char* argv[])
+{
+	struct lptpwm* pwm;
+	float val[NUMCH] = {0.1f, 0.1f, 0.1f, 0.1f};
+	int i,j;
+
+	pwm = lptpwm_open(FREQ, NUMCH, -1);
+	if (!pwm) {
+		fprintf(stderr, "OPening the port failed\n");
+		return 1;
+	}
+
+		lptpwm_setlevels(pwm, val);
+	for (i=0; i<20; i++) {
+	//	for (j=0; j<NUMCH; j++) {
+	//		val[j] = 0.5f;
+	//	}
+	//	lptpwm_setlevels(pwm, val);
+		sleep(1);
+	}
+
+
+	lptpwm_close(pwm);
+
+	return 0;
+}
